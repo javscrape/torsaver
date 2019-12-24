209ab56958818a6cd77f2b9f8ce8723f50a45624
godcong <jumbycc@163.com>
2019-12-25 02:10:26 +0800

    update torsaver

--- log.go
+++ log.go
@@ -1,12 +1,10 @@
 package torsaver
 
 import (
-	"github.com/goextension/log"
 	"github.com/goextension/log/zap"
 )
 
 func init() {
 	zap.DefaultZapFilePath = "tor.log"
-
-	log.Register(zap.NewZapFileSugar())
+	zap.InitZapFileSugar()
 }
